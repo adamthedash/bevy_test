b2849a5a6f3634d10755b0a9287deaea2b10d4d7
adamthedash <adamthedash2@gmail.com>
2024-12-27 02:35:45 +0000

    Initial commit

---
+++ src/assets.rs
@@ -0,0 +1,85 @@
+use std::collections::HashMap;
+use std::ops::{Deref, DerefMut};
+
+use bevy::prelude::*;
+
+/// Used to store meshes which are re-used by instances of objects.
+#[derive(Resource)]
+pub struct MeshStore(pub HashMap<String, Handle<Mesh>>);
+
+impl MeshStore {
+    pub fn new() -> Self {
+        Self(HashMap::new())
+    }
+}
+
+impl Deref for MeshStore {
+    type Target = HashMap<String, Handle<Mesh>>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for MeshStore {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+/// Used to store colours which are re-used by instances of objects.
+#[derive(Resource)]
+pub struct MaterialStore(pub HashMap<String, Handle<ColorMaterial>>);
+
+impl MaterialStore {
+    pub fn new() -> Self {
+        Self(HashMap::new())
+    }
+}
+
+impl Deref for MaterialStore {
+    type Target = HashMap<String, Handle<ColorMaterial>>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for MaterialStore {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+pub fn load_entity_meshes(
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut mesh_store: ResMut<MeshStore>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut material_store: ResMut<MaterialStore>,
+) {
+    // Entities
+    println!("Loading meshes");
+    let square = Rectangle::new(1., 1.);
+    mesh_store.insert("entity_wall".to_string(), meshes.add(square));
+    mesh_store.insert("entity_floor".to_string(), meshes.add(square));
+    mesh_store.insert("entity_player".to_string(), meshes.add(square));
+    mesh_store.insert("entity_enemy".to_string(), meshes.add(square));
+
+    println!("Loading colors");
+    material_store.insert(
+        "entity_wall".to_string(),
+        materials.add(Color::srgb(0.25, 0.25, 0.25)),
+    );
+    material_store.insert(
+        "entity_floor".to_string(),
+        materials.add(Color::srgb(0.75, 0.75, 0.75)),
+    );
+    material_store.insert(
+        "entity_player".to_string(),
+        materials.add(Color::srgb(0., 0.75, 0.)),
+    );
+    material_store.insert(
+        "entity_enemy".to_string(),
+        materials.add(Color::srgb(0.75, 0., 0.)),
+    );
+}
---
+++ src/hello.rs
@@ -0,0 +1,42 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_systems(Startup, add_people)
+            .add_systems(Update, (greet_people, update_people).chain());
+    }
+}
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Adam".to_string())));
+    commands.spawn((Person, Name("Rob".to_string())));
+}
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("hello {}!", name.0);
+        }
+    }
+}
+
+fn update_people(mut query: Query<&mut Name, With<Person>>) {
+    for mut name in &mut query {
+        if name.0 == "Rob" {
+            name.0 = "Bob".to_string();
+            break; // We don't need to change any other names.
+        }
+    }
+}
